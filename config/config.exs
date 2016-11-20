06502f89b19f3854b3edea85872cb42f48aaebdc
Timmo Verlaan <tverlaan@gmail.com>
2016-11-20 21:05:45 +0100

    Update config.exs structure

--- config/config.exs
+++ config/config.exs
@@ -2,32 +2,11 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure for your application as:
-#
-#     config :brew_ex, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:brew_ex, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
-
 config :nerves, :firmware,
-  fwup_conf: "config/fwup.conf"
+  fwup_conf: "config/#{Mix.Project.config[:target]}/fwup.conf"
+
+config :brew, :temperature,
+  high: 21,
+  low: 20
+
+import_config "config.wifi.exs"
